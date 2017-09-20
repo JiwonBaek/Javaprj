a04223f6fd122804abd15a0d00007f40b1cbf699
user <user@user-pc>
2017-09-20 09:52:21 +0900

    dd

--- src/rtti/MyDispatcher.java
+++ src/rtti/MyDispatcher.java
@@ -1,5 +1,7 @@
 package rtti;
 
+import java.lang.reflect.Method;
+
 public class MyDispatcher {
 
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
@@ -7,6 +9,10 @@ public class MyDispatcher {
 		MyClass myClass = (MyClass)Class.forName("rtti.MyClass").newInstance();
 		
 		myClass.print();
+		
+		 Method[] methods=myClass.getClass().getMethods();
+		 
+		 System.out.println(methods.length);
 
 	}
 
